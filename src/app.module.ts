4c91804f7d884ca0cbafbdced348d122a22110c5
Shabalin-Daniil <daniilsabalin68@gmail.com>
2024-08-16 19:04:13 +0300

    feat: дополнен модуль приложения для всех модулей

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './user/user.module';
+import { User } from './user/user.entity';
+import { ProductModule } from './product/product.module';
+import { Product } from './product/product.entity';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { CategoryModule } from './category/category.module';
+import { Category } from './category/category.entity';
+import { Order } from './order/order.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Product, Order, Category],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+    ProductModule,
+    OrderModule,
+    CategoryModule
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Настройка Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API документация')
+    .setDescription('Документация для API интернет-магазина')
+    .setVersion('1.0')
+    .addTag('products') 
+    .addTag('categories')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
   await app.listen(3000);
 }
 bootstrap();
